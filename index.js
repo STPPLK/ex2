3285e4c0dde0be5c831cf77ca3e5b7c481831ff8
Sudthipong Pralakam <59143448@g.cmru.ac.th>
2019-07-13 14:13:50 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ app.get('/', (req, res) => res.send('Sudthipong Pralakam'))
 app.get('/about', (req, res) => res.send('เกี่ยวกับเรา'))
 app.get('/contact', (req, res) => res.send('ติดต่อเรา'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
